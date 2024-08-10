d464c769b1209ec17339d4bf3a6a63518a7d2f93
Frank0125 <a01252974@tec.mx>
2024-08-09 18:08:30 -0700

    Functional in Pyhton - Corrections in rules and update function

--- GOL.py
+++ GOL.py
@@ -5,7 +5,7 @@ import matplotlib.animation as animation
 # Genera el grid de size dims, y de forma aleatoria pone cuales 
 # celdas estan vivas o no al inicio
 def create_grid(dims, alive_probability=0.2, seed=None):
-    if seed:
+    if seed is not None:
         np.random.seed(seed)
     return np.random.choice([False, True], size=dims, p=[1 - alive_probability, alive_probability])
 
@@ -15,27 +15,28 @@ def count_alive_neighbors(grid, x, y):
     alive_neighbors = np.sum(neighborhood) - grid[x, y]
     return alive_neighbors
 
-def step(grid):
+def step(grid, rules):
     new_grid = grid.copy()
     for x in range(grid.shape[0]):
         for y in range(grid.shape[1]):
             alive_neighbors = count_alive_neighbors(grid, x, y)
-            if grid[x, y]: # Si la celda está viva
-                if alive_neighbors < 2 or alive_neighbors > 3:
-                    new_grid[x, y] = False # Muere por bajo o alto num de vecinos
-            else: # Activa la celda como viva cuando los vecinos son 3
-                if alive_neighbors == 3:
-                    new_grid[x, y] = True
+            if grid[x, y] and rules[0] <= alive_neighbors <= rules[1]: 
+               new_grid[x, y] = True # La celda sobrevive
+            elif not grid[x, y] and rules[2] <= alive_neighbors <= rules[3]:
+                new_grid[x, y] = True # La celda nace
+            else:
+                new_grid[x,y] = False # La celda muere
     return new_grid
 
 # Actualiza la ventana con las nuevas posiciones de las celdas
-def update(frame, img, grid):
-    new_grid = step(grid)
+def update(frame, img, grid, rules):
+    new_grid = step(grid, rules)
     img.set_data(new_grid)
     grid[:] = new_grid[:] 
     return [img]
 
 # Variables de inicio
+rules = (2, 3, 3, 3) # (D, S, R, O)
 dims = (50, 50)
 alive_probability = 0.2
 seed = 313
@@ -45,6 +46,6 @@ grid = create_grid(dims, alive_probability, seed)
 fig, ax = plt.subplots()
 img = ax.imshow(grid, cmap='gray', interpolation='nearest')
 
-ani = animation.FuncAnimation(fig, update, fargs=(img, grid), frames=60, interval=100, save_count=50)
+ani = animation.FuncAnimation(fig, update, fargs=(img, grid, rules), frames=60, interval=100, save_count=50)
 
 plt.show()
